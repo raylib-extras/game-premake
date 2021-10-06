4f4a48361b89bec1421939832c60659a0fa8ae52
Jeffery Myers <jeffm2501@gmail.com>
2021-10-06 16:41:12 -0700

    premake code

---
+++ premake5.lua
@@ -0,0 +1,85 @@
+workspace "YourGame"
+	configurations { "Debug","Debug.DLL", "Release", "Release.DLL" }
+	platforms { "x64"}
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+		
+	filter "configurations:Debug.DLL"
+		defines { "DEBUG" }
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"	
+		
+	filter "configurations:Release.DLL"
+		defines { "NDEBUG" }
+		optimize "On"	
+		
+	filter { "platforms:x64" }
+		architecture "x86_64"
+		
+	targetdir "bin/%{cfg.buildcfg}/"
+	
+	defines{"PLATFORM_DESKTOP", "GRAPHICS_API_OPENGL_33"}
+		
+project "raylib"
+		filter "configurations:Debug.DLL OR Release.DLL"
+			kind "SharedLib"
+			defines {"BUILD_LIBTYPE_SHARED"}
+			
+		filter "configurations:Debug OR Release"
+			kind "StaticLib"
+			
+		filter "action:vs*"
+			defines{"_WINSOCK_DEPRECATED_NO_WARNINGS", "_CRT_SECURE_NO_WARNINGS", "_WIN32"}
+			links {"winmm"}
+			
+		filter "action:gmake*"
+			links {"pthread", "GL", "m", "dl", "rt", "X11"}
+			
+		filter{}
+		
+		location "build"
+		language "C++"
+		targetdir "bin/%{cfg.buildcfg}"
+		cppdialect "C++17"
+		
+		includedirs { "raylib/src", "raylib/src/external/glfw/include"}
+		vpaths 
+		{
+			["Header Files"] = { "raylib/src/**.h"},
+			["Source Files/*"] = {"raylib/src/**.c"},
+		}
+		files {"raylib/src/*.h", "raylib/src/*.c"}
+		
+project "YourGame"
+	kind "ConsoleApp"
+	location "%{wks.name}"
+	language "C++"
+	targetdir "bin/%{cfg.buildcfg}"
+	cppdialect "C++17"
+	
+	includedirs {"src"}
+	vpaths 
+	{
+		["Header Files"] = { "**.h"},
+		["Source Files"] = {"**.c", "**.cpp"},
+	}
+	files {"%{wks.name}/**.c", "%{wks.name}/**.cpp", "%{wks.name}/**.h"}
+
+	links {"raylib"}
+	
+	includedirs { "%{wks.name}", "raylib/src" }
+	defines{"PLATFORM_DESKTOP", "GRAPHICS_API_OPENGL_33"}
+	
+	filter "action:vs*"
+		defines{"_WINSOCK_DEPRECATED_NO_WARNINGS", "_CRT_SECURE_NO_WARNINGS", "_WIN32"}
+		dependson {"raylib"}
+		links {"raylib.lib", "winmm", "kernel32"}
+		libdirs {"bin/%{cfg.buildcfg}"}
+		
+	filter "action:gmake*"
+		links {"pthread", "GL", "m", "dl", "rt", "X11"}

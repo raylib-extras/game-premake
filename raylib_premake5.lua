9db5ecec7985c93a5dc0439081ddcd62e9c3ab8e
Jeffery Myers <jeffm2501@gmail.com>
2022-06-04 12:57:11 -0700

    remove duplicate kernel32 link

--- raylib_premake5.lua
+++ raylib_premake5.lua
@@ -65,7 +65,7 @@ function link_raylib()
 
     filter "system:windows"
         defines{"_WIN32"}
-        links {"winmm", "kernel32", "opengl32", "kernel32", "gdi32"}
+        links {"winmm", "kernel32", "opengl32", "gdi32"}
         libdirs {"../_bin/%{cfg.buildcfg}"}
 
     filter "system:linux"
